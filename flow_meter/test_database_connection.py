bd5ccb83ebb8a9a06fe499d38057f044d7598d19
kromerh <heiko.kromer@gmail.com>
2019-05-07 00:50:38 +0200

    test the database

---
+++ flow_meter/test_database_connection.py
@@ -0,0 +1,50 @@
+from time import sleep
+import serial
+import sys
+import pandas as pd
+import pymysql
+
+mysql_connection = pymysql.connect(host="twofast-RPi3-0",  # your host
+					 user="writer",  # username
+					 passwd="heiko",  # password
+					 db="NG_twofast_DB", # name of the database
+					charset='utf8',
+					cursorclass=pymysql.cursors.DictCursor)  
+
+def getAvailableHistoricalDates():
+
+	# DOSE
+	query = "SELECT DISTINCT DATE(time) FROM data_dose"
+	df = pd.read_sql(query, mysql_connection)
+
+	print(df.head())
+
+
+
+print(getAvailableHistoricalDates())
+
+sys.exit()
+
+
+
+arduinoPort = '/dev/ttyACM0'
+ser = serial.Serial(arduinoPort, 9600)
+sleep(1)
+val = 0.5 # Below 32 everything in ASCII is gibberish
+while True:
+	try:
+		valueSend = str(val)
+		ser.write(valueSend.encode()) # Convert the decimal number to ASCII then send it to the Arduino
+		print(valueSend.encode())
+		sleep(1) # Delay 
+		valueRead = ser.readline(500)
+
+		print(valueRead) # Read the newest output from the Arduino
+		sleep(0.5) # Delay 
+		ser.flushInput()  #flush input buffer, discarding all its contents
+		ser.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
+	except KeyboardInterrupt:
+		print('Ctrl + C. Exiting. Flushing serial connection.')
+		ser.flushInput()  #flush input buffer, discarding all its contents
+		ser.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
+		sys.exit(1)
