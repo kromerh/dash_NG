5dae4442fcf9980efeb95cd5494a82b793025656
Kromer Heiko <kromer_h@psi.ch>
2019-06-27 12:36:39 +0200

    test connection

--- flow_meter/test_serial_connection.py
+++ flow_meter/test_serial_connection.py
@@ -4,7 +4,4 @@ import serial.tools.list_ports
 arduinoPort = '/dev/ttyACM0'  # might need to be changed if another arduino is plugged in or other serial
 
 
-def fun_read_serial_ports():
-    return list(serial.tools.list_ports.comports())
-
-print(fun_read_serial_ports())
+print([comport.device for comport in serial.tools.list_ports.comports()])
