6a7b4e67019cf45e04792d813461addfbc43b366
Kromer Heiko <kromer_h@psi.ch>
2019-12-09 15:50:42 +0100

    motor control

--- V3/callbacks_microwave.py
+++ V3/callbacks_microwave.py
@@ -32,41 +32,6 @@ connect_string = 'mysql+pymysql://%(user)s:%(pw)s@%(host)s/%(db)s'% {"user": use
 sql_engine = sql.create_engine(connect_string)
 
 
-SP_P_max =  200.0  # Setpoint maximal power
-SP_F_min = 2400.0 # Setpoint minimum frequency
-SP_F_max = 2500.0 # Setpoint maximum frequency (steps of 100 kHz)
-d_F = 0.1 #
-
-# DATABASE
-
-def read_power_table(sql_engine, pastSeconds=60): # read past 60secs by default
-	"""
-	Read the power table from the database
-	"""
-	query = "SELECT * FROM microwave_generator_power ORDER BY id DESC LIMIT %(pastSeconds)s" % {"pastSeconds": pastSeconds}
-	df = pd.read_sql(query, sql_engine)
-	# print(df)
-	return df
-
-def read_frequency_table(sql_engine, pastSeconds=60): # read past 60secs by default
-	"""
-	Read the power table from the database
-	"""
-	query = "SELECT * FROM microwave_generator_frequency ORDER BY id DESC LIMIT %(pastSeconds)s" % {"pastSeconds": pastSeconds}
-	df = pd.read_sql(query, sql_engine)
-
-	return df
-
-def read_state_table(sql_engine, pastSeconds=60): # read past 60secs by default
-	"""
-	Read the power table from the database
-	"""
-	query = "SELECT * FROM microwave_generator_state ORDER BY id DESC LIMIT %(pastSeconds)s" % {"pastSeconds": pastSeconds}
-	df = pd.read_sql(query, sql_engine)
-
-	return df
-
-
 def MWButtonControl(sql_engine, value):
 	timeNow = datetime.datetime.now()
 	qry = "INSERT INTO microwave_motor_command (time_created, command, executed) VALUES (%(time)s, \"%(value)s\", 0)" % {"time": timeNow, "value": value}
@@ -74,307 +39,69 @@ def MWButtonControl(sql_engine, value):
 
 	
 
-
 # CALLBACKS
 
-# Start the microwave button
-@app.callback(
-	Output("microwave-status-monitor", "value"),
-	[Input("btn-mw-motor-on", "n_clicks")]
-)
-def btn_mw_on(n_clicks):
-	if n_clicks:
-		sleep(3)
-		print(f'Starting MW, n_clicks: {n_clicks}')
-		value = "88,800"
-		print(f'Sending {value}')
-		
-		MWButtonControl(sql_engine, value)
-				
-		status = (
-			"-----------STATUS------------\n"
-			+ "Sending to mw motor: " + str(value) + "\n"
-		)
-		sleep(3)
-		value = "92,1100"
-		print(f'Sending {value}')
-		
-		MWButtonControl(sql_engine, value)
-		status = (
-			status 
-			+ "Sending to mw motor: " + str(value) + "\n"
-		)
-
-		return status
-
-
-# Stop the microwave button
-@app.callback(
-	Output("microwave-status-monitor", "value"),
-	[Input("btn-mw-motor-off", "n_clicks")]
-)
-def btn_mw_off(n_clicks):
-	if n_clicks:
-		sleep(3)
-		print(f'Stopping MW, n_clicks: {n_clicks}')
-		value = "92,1100"
-		print(f'Sending {value}')
-		
-		MWButtonControl(sql_engine, value)
-				
-		status = (
-			"-----------STATUS------------\n"
-			+ "Sending to mw motor: " + str(value) + "\n"
-		)
-		sleep(3)
-		value = "88,800"
-		print(f'Sending {value}')
-		
-		MWButtonControl(sql_engine, value)
-		status = (
-			status 
-			+ "Sending to mw motor: " + str(value) + "\n"
-		)
-		
-		return status
-
-
-
-# # Textarea Communication
+# # status of MW motor
+# # Stop the microwave button
 # @app.callback(
 # 	Output("microwave-status-monitor", "value"),
-# 	[Input("microwave-readout-interval", "n_intervals")],
-# 	[State("microwave-power-values", "children")]
+# 	[Input("microwave-MW-button-off", "children")],
+# 	[State("microwave-MW-button-on", "children")]
 # )
-# def microwave_text_area(intervals,
-# 	json_data
-# 	):
-# 	last_db_reading = -1
-# 	try:
-# 		df = pd.read_json(json_data, orient='split')
-# 		if len(df) > 0:
-# 			last_db_reading = df.loc[0,'time']
-# 			last_db_reading = pd.to_datetime(last_db_reading)
-# 			last_db_reading = last_db_reading.strftime("%Y-%m-%d, %H:%M:%S")
-# 		else:
-# 			last_db_reading = -1
-# 	except:
-# 		pass
-
-
+# def mw_motor_status(status_off, status_on):
 # 	status = (
-# 		"-----------STATUS------------\n"
-# 		+ "Last reading microwave power (FP) received: " + str(last_db_reading) + "\n"
+# 	"-----------STATUS------------\n"
+# 	+ "microwave-MW-button-off: " + str(status_off) + "\n"
+# 	+ "microwave-MW-button-on: " + str(status_on) + "\n"
 # 	)
-
 # 	return status
 
+# # Start the microwave button
+# @app.callback(
+# 	# Output("microwave-status-monitor", "value"),
+# 	Output("microwave-MW-button-on", "children"),
+# 	[Input("btn-mw-motor-on", "n_clicks")]
+# )
+# def btn_mw_on(n_clicks):
+# 	if n_clicks:
+# 		sleep(3)
+# 		print(f'Starting MW, n_clicks: {n_clicks}')
+# 		value = "88,800"
+# 		print(f'Sending {value}')
+		
+# 		MWButtonControl(sql_engine, value)
+				
+# 		sleep(3)
+# 		value = "92,1100"
+# 		print(f'Sending {value}')
+		
+# 		MWButtonControl(sql_engine, value)
+# 		status = "ON"
 
-# microwave-power-values
-# callback to read the database and store in a json objective
-@app.callback(
-	Output('microwave-power-values', 'children'),
-	[Input('microwave-readout-interval', 'n_intervals')]
-	)
-def cb_read_power_table(intervals):
-	df = read_power_table(sql_engine, 120)  # retrieve the past 60 seconds
-	return df.to_json(date_format='iso', orient='split')
-
-
-# microwave-frequency-values
-# callback to read the database and store in a json objective
-@app.callback(
-	Output('microwave-frequency-values', 'children'),
-	[Input('microwave-readout-interval', 'n_intervals')]
-	)
-def cb_read_frequency_table(intervals):
-	df = read_frequency_table(sql_engine, 120)  # retrieve the past 60 seconds
-	return df.to_json(date_format='iso', orient='split')
-
-# microwave-MW-state-values
-# callback to read the database and store in a json objective
-@app.callback(
-	Output('microwave-MW-state-values', 'children'),
-	[Input('microwave-readout-interval', 'n_intervals')]
-	)
-def cb_read_state_table(intervals):
-	df = read_state_table(sql_engine, 120)  # retrieve the past 60 seconds
-	return df.to_json(date_format='iso', orient='split')
+# 		return status
 
 
+# # Stop the microwave button
+# @app.callback(
+# 	Output("microwave-MW-button-off", "children"),
+# 	[Input("btn-mw-motor-off", "n_clicks")]
+# )
+# def btn_mw_off(n_clicks):
+# 	if n_clicks:
+# 		sleep(3)
+# 		print(f'Stopping MW, n_clicks: {n_clicks}')
+# 		value = "92,1100"
+# 		print(f'Sending {value}')
+		
+# 		MWButtonControl(sql_engine, value)
+				
 
-
-
-
-# microwave power graph
-@app.callback(
-	Output("microwave-power-graph", "figure"),
-	[Input("microwave-power-values", "children")]
-)
-# def plot_graph_data(df, figure, command, start, start_button, PID):
-def cb_plot_power_graph(json_data):
-
-	# print(state_dic)
-	traces = []
-
-	try:
-		data = pd.read_json(json_data, orient='split')
-		# print(data.head())
-		traces.append(
-			go.Scatter(
-				x=data['time'],
-				y=data['FP'],
-				line=go.scatter.Line(
-					color='#42C4F7',
-					# width=2.0
-				),
-				text='Forward power [W]',
-				# mode='markers',
-				mode='lines',
-				opacity=1,
-
-				name='FP [W]'
-			)
-		)
-		traces.append(
-			go.Scatter(
-				x=data['time'],
-				y=data['RP'],
-				line=go.scatter.Line(
-					color='red',
-					# width=2.0
-				),
-				text='Reflected power [W]',
-				# mode='markers',
-				mode='lines',
-				opacity=1,
-
-				name='RP [W]'
-			)
-		)
-		traces.append(
-			go.Scatter(
-				x=data['time'],
-				y=data['power_setpoint'],
-				line=go.scatter.Line(
-					color='green',
-					# width=2.0
-				),
-				text='Setpoint power [W]',
-				# mode='markers',
-				mode='lines',
-				opacity=1,
-
-				name='SP [W]'
-			)
-		)
-
-	except:
-		traces.append(go.Scatter(
-			x=[],
-			y=[],
-			line=go.scatter.Line(
-				color='#42C4F7',
-				width=1.0
-			),
-			text='dose [muSv/hr]',
-			# mode='markers',
-			opacity=1,
-			marker={
-				 'size': 15,
-				 'line': {'width': 1, 'color': '#42C4F7'}
-			},
-			mode='lines',
-			name='dose [muSv/hr]'
-		))
-
-	return {
-		'data': traces,
-		'layout': go.Layout(
-			xaxis={'title': 'Time'},
-			yaxis={'title': 'Power [W]', 'range':[0,205]},
-			margin={'l': 100, 'b': 100, 't': 10, 'r': 10},
-			legend={'x': 0, 'y': 1},
-			hovermode='closest'
-		)
-	}
-
-
-
-# microwave frequency graph
-@app.callback(
-	Output("microwave-frequency-graph", "figure"),
-	[Input("microwave-frequency-values", "children")]
-)
-# def plot_graph_data(df, figure, command, start, start_button, PID):
-def cb_plot_frequency_graph(json_data):
-
-	# print(state_dic)
-	traces = []
-
-	try:
-		data = pd.read_json(json_data, orient='split')
-		# print(data.head())
-		traces.append(
-			go.Scatter(
-				x=data['time'],
-				y=data['frequency'],
-				line=go.scatter.Line(
-					color='#42C4F7',
-					# width=2.0
-				),
-				text='Frequency [MHz]',
-				# mode='markers',
-				mode='lines',
-				opacity=1,
-
-				name='FP [W]'
-			)
-		)
-		traces.append(
-			go.Scatter(
-				x=data['time'],
-				y=data['frequency_setpoint'],
-				line=go.scatter.Line(
-					color='green',
-					# width=2.0
-				),
-				text='Setpoint frequency [MHz]',
-				# mode='markers',
-				mode='lines',
-				opacity=1,
-
-				name='SP [W]'
-			)
-		)
-
-	except:
-		traces.append(go.Scatter(
-			x=[],
-			y=[],
-			line=go.scatter.Line(
-				color='#42C4F7',
-				width=1.0
-			),
-			text='dose [muSv/hr]',
-			# mode='markers',
-			opacity=1,
-			marker={
-				 'size': 15,
-				 'line': {'width': 1, 'color': '#42C4F7'}
-			},
-			mode='lines',
-			name='dose [muSv/hr]'
-		))
-
-	return {
-		'data': traces,
-		'layout': go.Layout(
-			xaxis={'title': 'Time'},
-			yaxis={'title': 'Frequency [MHz]', 'range':[2300,2600]},
-			margin={'l': 100, 'b': 100, 't': 10, 'r': 10},
-			legend={'x': 0, 'y': 1},
-			hovermode='closest'
-		)
-	}
+# 		sleep(3)
+# 		value = "88,800"
+# 		print(f'Sending {value}')
+		
+# 		MWButtonControl(sql_engine, value)
+# 		status = "OFF"
+		
+# 		return status
 
