af6b3783f398e022f01fe02036e89c2c766add20
Heiko Kromer <heiko.kromer@gmail.com>
2020-01-07 15:03:18 +0100

    MODBUS test

--- microwave/MODBUS_test.py
+++ microwave/MODBUS_test.py
@@ -1,12 +1,12 @@
 from pyModbusTCP.client import ModbusClient
 try:
-    c = ModbusClient(host="localhost", port=502, auto_open=True, auto_close=True)
+    c = ModbusClient(host="twofast-rpi3-5.local.mdns", port=502, auto_open=True, auto_close=True)
 except ValueError:
     print("Error with host or port params")
 
 for ii in range(104,118):
 	returnlist = []
-	for jj in range(1,126):
+	for jj in range(0,126):
 		regs_list_1 = c.read_holding_registers(ii, jj)
 		returnlist.append(regs_list_1)
 	print(ii, returnlist)
