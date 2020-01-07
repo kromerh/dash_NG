b8318d0a1561862c1c1bc3808adc2b09b8fe3779
Heiko Kromer <heiko.kromer@gmail.com>
2020-01-07 15:05:11 +0100

    MODBUS test

--- microwave/MODBUS_test.py
+++ microwave/MODBUS_test.py
@@ -4,9 +4,10 @@ try:
 except ValueError:
     print("Error with host or port params")
 
-for ii in range(104,118):
+for ii in range(0,65535):
 	returnlist = []
-	for jj in range(0,126):
+	for jj in range(0,2000):
 		regs_list_1 = c.read_holding_registers(ii, jj)
 		returnlist.append(regs_list_1)
-	print(ii, returnlist)
+	if not None in returnlist:
+		print(ii, returnlist)
