a3585a6f7c5fdf75736b747b775426fb65ee6a7f
Heiko Kromer <heiko.kromer@gmail.com>
2020-01-08 15:29:21 +0100

    write coil

--- microwave/MODBUS_test.py
+++ microwave/MODBUS_test.py
@@ -36,7 +36,7 @@ for ii in range(104,110):
 print('\n Writing coil')
 bit_addr = 2
 bit_value = 128 # 0 0 0 0 0 0 0 1
-write_single_coil(bit_addr, bit_value)
+c.write_single_coil(bit_addr, bit_value)
 
 print('\n Reading registers')
 for ii in range(104,110):
