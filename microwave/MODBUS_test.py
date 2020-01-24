f292d67d8f46a10bf4176ce8a2af10a5d17fe76e
Heiko Kromer <heiko.kromer@gmail.com>
2020-01-24 10:04:33 +0100

    modbust test

--- microwave/MODBUS_test.py
+++ microwave/MODBUS_test.py
@@ -6,7 +6,7 @@ from pyModbusTCP.client import ModbusClient
 # sudo
 try:
     # c = ModbusClient(host="169.254.150.42", port=502, auto_open=True, auto_close=True)
-    c = ModbusClient(host="169.254.240.201", port=502, auto_open=True, auto_close=True)
+    c = ModbusClient(host="169.254.240.116", port=502, auto_open=True, auto_close=True)
     # c = ModbusClient(host="169.254.240.1", port=502, auto_open=True, auto_close=True)
 except ValueError:
     print("Error with host or port params")
@@ -19,36 +19,117 @@ except ValueError:
 # 	if not None in returnlist:
 # 		print(ii, returnlist)
 
+# bool to store if all the settings are set
+RAMP_SET = False
+RAMP_TIME_SET = False
+FP_SET = False
+MODE_SET = False
+
+def send_heartbeat(ModbusClient):
+	# sends MODBUS heart beat
+	# c is ModbusClient
+	wr = ModbusClient.write_single_register(20, 128) # modbus heartbeat
+
+def set_start_mode_ramp(ModbusClient):
+	# Sets the start mode to ramp
+	# c is ModbusClient
+	wr = ModbusClient.write_single_coil(2,2)
+	print(f'set_start_mode_ramp: {wr}')
+	RAMP_SET = True
+
+def set_start_time(ModbusClient):
+	# Sets the start time to 60s
+	# c is ModbusClient
+	wr = ModbusClient.write_single_coil(4,200)
+	print(f'set_start_time: {wr}')
+	RAMP_TIME_SET = True
+
+def set_FW_power(ModbusClient):
+	# Sets the forward power set point to 200 W
+	# c is ModbusClient
+	wr = ModbusClient.write_single_coil(0,200)
+	print(f'set_FW_power: {wr}')
+	FP_SET = True
+
+def set_microwave_mode(ModbusClient):
+	# Sets the microwave mode:
+	#	autotuning on, reflected power RP limitation, reset faults
+	# c is ModbusClient
+	bit_addr = 2
+	bit_value = 146 # 0 1 0 0 1 0 0 1
+	wr = c.write_single_register(bit_addr, bit_value)
+	print(f'set_microwave_mode: {wr}')
+	MODE_SET = True
 
 
-# for ii in range(100,120):
-# 	returnlist = []
-# 	for jj in range(0,20):
-# 		regs_list_1 = c.read_holding_registers(ii, jj)
-# 		returnlist.append(regs_list_1)
-# 	print(ii, returnlist)
 
-print('Reading registers')
-for ii in range(104,110):
-	regs_list_1 = c.read_holding_registers(ii, 1)
-	print(ii, regs_list_1)
+def read_fault_present(ModbusClient):
+	# reads if fault present
+	r0 = c.read_holding_registers(105, 1)
+	if (len(r0) > 0) and (r0[0] == 128):
+		# something returned and message is Moduel Ready for Microwaves
+		print(f'Ready for microwaves {r0}')
+	else:
+		print(f'Fault present: {r0}')
+		# read the type of fault
+		r1 = c.read_holding_registers(104, 1)
+		print(f'Type of fault: {r1}')
 
-print('\n Writing coil')
-bit_addr = 2
-bit_value = 128 # 0 0 0 0 0 0 0 1
-wr = c.write_single_register(bit_addr, bit_value)
-print(wr)
+def read_FP(ModbusClient):
+	# reads forward power
+	r0 = c.read_holding_registers(102, 10)
+	print(f'read_FP {r0}')
 
-print('\n Reading registers')
-for ii in range(104,110):
-	regs_list_1 = c.read_holding_registers(ii, 1)
-	print(ii, regs_list_1)
+def read_RP(ModbusClient):
+	# reads reflected power
+	r0 = c.read_holding_registers(103, 10)
+	print(f'read_RP {r0}')
+
+def read_set_FP(ModbusClient):
+	# reads setpoint power
+	r0 = c.read_holding_registers(100, 10)
+	print(f'read_set_FP {r0}')
+
+def read_freq(ModbusClient):
+	# reads current frequency
+	r0 = c.read_holding_registers(112, 10)
+	print(f'read_freq {r0}')
+
+
 
 while True:
-	wr = c.write_single_register(20, 128) # modbus heartbeat
-	rr = c.read_holding_registers(99, 1)
-	r0 = c.read_holding_registers(105, 1)
-	print(r0)
+	# sent hearbeat
+	send_heartbeat(c)
+
+	# set start mode to tamp
+	if RAMP_SET == False:
+		set_start_mode_ramp(c)
+
+	# set start time 60 s
+	if RAMP_TIME_SET == False:
+		set_start_time(c)
+
+	# set the forward power set point to 200 W
+	if FP_SET == False:
+		set_FW_power(c)
+
+
+	# set the microwave mode:
+	if set_microwave_mode == False:
+		set_FW_power(c)
+
+	read_fault_present(c)
+	read_FP(c)
+	read_RP(c)
+	read_set_FP(c)
+	read_freq(c)
+
+
+# while True:
+# 	wr = c.write_single_register(20, 128) # modbus heartbeat
+# 	rr = c.read_holding_registers(99, 1)
+# 	r0 = c.read_holding_registers(105, 1)
+# 	print(r0)
 
 # addr = 104
 # regs_list_1 = c.read_holding_registers(addr, 20)
